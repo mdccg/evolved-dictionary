3b46bdfa5f65e723f204bb063da952c8bcff2dbf
Lab 3 <matheusdanielgomess@gmail.com>
2023-04-20 19:31:57 -0400

    Pro Zé não ficar triste

--- cypress/e2e/app.cy.ts
+++ cypress/e2e/app.cy.ts
@@ -1,3 +1,63 @@
+interface Word {
+  term: string;
+  audioUrls: string[];
+  meanings: string[];
+}
+
+interface WordNotFound {
+  title: string;
+}
+
+const isInstanceOfWordNotFound = (obj: Word[] | WordNotFound) => (<WordNotFound>obj).title !== undefined;
+
+const getWords = (jsonObj: any): Word[] | WordNotFound => {
+  const { title } = jsonObj;
+
+  if (title) {
+    const notFound: WordNotFound = { title };
+    return notFound;
+  }
+
+  const words: Word[] = [];
+
+  jsonObj.forEach((obj: any) => {
+    const { word, phonetics, meanings } = obj;
+
+    const audioUrls: string[] = [];
+    if (phonetics && phonetics.length > 0) {
+      phonetics.forEach((p: any) => {
+        const { audio } = p;
+        if (audio) {
+          audioUrls.push(audio);
+        }
+      })
+    }
+
+    const meaningsArr: string[] = [];
+    if (meanings && meanings.length > 0) {
+      meanings.forEach((m: any) => {
+        const { definitions } = m;
+        if (definitions && definitions.length > 0) {
+          definitions.forEach((d: any) => {
+            const { definition } = d;
+            meaningsArr.push(definition);
+          });
+        }
+      });
+    }
+
+    const wordObj: Word = {
+      term: word,
+      audioUrls,
+      meanings: meaningsArr,
+    };
+    words.push(wordObj);
+  });
+
+  return words;
+}
+// Criar uma task para todo o código acima
+
 describe('English Dictionary E2E tests', () => {
   beforeEach(() => {
     cy.visit('');
@@ -10,5 +70,22 @@ describe('English Dictionary E2E tests', () => {
     cy.intercept('GET', `${Cypress.env('API_URL')}/${query}`, { body: this.apiResponse });
     cy.performSearch(query);
     cy.get('[data-cy="word-card"]').should('have.length', this.apiResponse.length);
+  });
+
+  it('should show correct meanings about a word', () => {
+    const query = 'word';
+
+    const requestOptions: Partial<Cypress.RequestOptions> = {
+      method: 'GET',
+      url: `${Cypress.env('API_URL')}/${query}`,
+      failOnStatusCode: false,
+    };
+
+    cy.request(requestOptions).then(({ status, body }) => {
+      const words: Word[] | WordNotFound = getWords(body);
+      cy.performSearch(query);
+      console.log(words);
+      cy.get('[data-cy="word-details"]').first().click();
+    });
   });
 });
